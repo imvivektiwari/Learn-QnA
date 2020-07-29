3deca37ebb86e05897b5cff0c0dc1542567bbc5b
Vivek Tiwari <vivektiwari.2896@gmail.com>
2020-07-30 00:36:52 +0530

    quiz form UI

--- src/components/mainQuiz/ChooseQuizForm.js
+++ src/components/mainQuiz/ChooseQuizForm.js
@@ -25,30 +25,51 @@ const ChooseQuizForm = ()=>{
         dispatch(setQuestionCount(value));
     };
 
+    const startQuiz = (event)=>{
+        event.preventDefault();
+    }
+    
     return (
-        <form id="quiz-form" className="debug">
-             <input  value={quizForm.numberOfQuestions} onChange={setNumberOfQuestions} />
-             <select value={quizForm.categories.selected.value} onChange={setCategory}>
+        <form id="quiz-form" onSubmit={startQuiz}>
+            <p className="quiz-form-title">Quiz Form</p>
+            <label>
+                <p>Number of Questions:</p>
+                <input  value={quizForm.numberOfQuestions} onChange={setNumberOfQuestions} />
+            </label>
+            <label>
+                <p>Select Category:</p>
+                <select value={quizForm.categories.selected.value} onChange={setCategory}>
                  {
                      quizForm.categories.categoryOptions.map((category)=>{
                         return <option value={category.value} key={category.value}>{category.text}</option>
                      })
                  }
              </select>
-             <select value={quizForm.difficultyLevels.selected.value} onChange={setDifficultyLevel}>
+            </label>
+             
+             <label>
+                <p>Select Difficulty:</p>
+                <select value={quizForm.difficultyLevels.selected.value} onChange={setDifficultyLevel}>
                  {
                      quizForm.difficultyLevels.difficultyLevelOptions.map((difficultyLevel)=>{
                         return <option value={difficultyLevel.value} key={difficultyLevel.value}>{difficultyLevel.text}</option>
                      })
                  }
              </select>
-             <select value={quizForm.types.selected.value} onChange={setQuizTypes}>
+            </label>
+           
+             <label>
+                <p>Select Type:</p>
+                <select value={quizForm.types.selected.value} onChange={setQuizTypes}>
                  {
                      quizForm.types.typeOptions.map((quizType)=>{
                         return <option value={quizType.value} key={quizType.value}>{quizType.text}</option>
                      })
                  }
              </select>
+            </label>
+           
+             <input type="submit" value="Start Quiz"/>
         </form>
     )
 };
